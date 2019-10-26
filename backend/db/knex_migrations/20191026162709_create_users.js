8babe508e092370fc677ee9f92fa2d46e8e708e1
tumiduong <duong.julie@live.com>
2019-10-26 16:38:43 +0000

    add migration for users

---
+++ backend/db/knex_migrations/20191026162709_create_users.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', t => {
+    t.increments('id')
+      .primary()
+      .unsigned();
+    t.string('first_name');
+    t.string('last_name');
+    t.string('email');
+    t.string('password');
+    t.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
+};
