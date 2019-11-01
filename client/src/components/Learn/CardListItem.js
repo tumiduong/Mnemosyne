70f136d809f61353813abec55a579d335b22cb37
berkererol <berkererol@gmail.com>
2019-11-01 08:02:29 -0400

    -B : Implemented onClick flipcard function, changed CardListItem.js and CardListItem.css

--- client/src/components/Learn/CardListItem.js
+++ client/src/components/Learn/CardListItem.js
@@ -1,42 +1,52 @@
-import React from "react";
-import './CardListItem.css'
+import React, {useState} from "react";
+import './CardListItem.css';
+
 
 export default function CardListItem(props) {
 
   let imgUrl = props.image;
-
   
-
   let divStyle = {
     backgroundImage: `url(${imgUrl})`,
     width: '100%',
     height: '100%',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    backgroundPosition:'center',
+    backgroundPosition: 'center',
     borderRadius: '0px 0px 20px 20px'
-
   };
-  
- 
+
+  const [ className, setClassName] = useState("card")
+
+  const switchClass = () => {
+    if (className === "card") {
+      setClassName("card flipped")
+    } else if (className === "card flipped") {
+      setClassName("card")
+    }
+  }
+
+
+
   return (
 
-    <div className="card-cover">
-      <div className="card-inner">
-        <div className="card-front">
-          <div className="card-cover-top-border"></div>
+    <div className="flipCard">
+      <div className={className} onClick={ () => switchClass()}>
+        <div className="side front">
+          <div className="card-front-top-border"> </div>
           <div className="card-term">
-            <p>{props.term}</p>
+            <p> {props.term} </p>
           </div>
+
         </div>
-
-
-        <div className="card-back">
-          <div className="card-back-top-border"></div>
+        <div className="side back">
+          <div className="card-back-top-border"> </div>
           <div className="card-definition">
-            <p>{props.definition}</p>
+            <p> {props.definition} </p>
           </div>
+
           <div style={divStyle}>
+
           </div>
         </div>
       </div>
